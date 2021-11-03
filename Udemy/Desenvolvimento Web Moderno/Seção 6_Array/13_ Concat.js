e0e8d11ec4276ed4d23e8d2b6c463bca4b9a9a23
Matheus Pereira <brasil.m.95@gmail.com>
2021-11-03 09:23:02 -0300

    Up aulas

---
+++ Udemy/Desenvolvimento Web Moderno/Seção 6_Array/13_ Concat.js
@@ -0,0 +1,9 @@
+// Gera um novo array, com base nos dados fornecidos 
+
+const filhas = ['valeskah', 'Cibalena']
+const filhos = ['Uoxinton', 'Uesclei']
+
+const todos = filhas.concat(filhos,'Fulano')
+console.log(todos)
+
+console.log([].concat([1,2],[2,3],5,[[5,6]]))
